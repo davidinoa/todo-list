060f4c2f51921670b70dc8e42558f78bdc593d29
David Inoa <davidinoa@gmail.com>
2018-01-24 10:53:28 -0500

    Add methods to delete and save a todo

--- scripts/app.js
+++ scripts/app.js
@@ -6,12 +6,31 @@ angular.module('todoListApp', [])
       $scope.todos = response.data;
     });
 
+    $scope.deleteTodo = function(todo, $index) {
+      dataService.deleteTodo(todo);
+      $scope.todos.splice($index, 1);
+    };
+
+    $scope.saveTodo = function(todo) {
+      dataService.saveTodo(todo);
+    };
+
   })
   .service('dataService', function($http) {
 
     this.getTodos = function(callback) {
       $http.get('mock/todos.json')
         .then(callback);
+    };
+
+    this.deleteTodo = function(todo) {
+      console.log('The ' + todo.name + ' todo has been deleted!');
+      // other logic
+    };
+
+    this.saveTodo = function(todo) {
+      console.log('The ' + todo.name + ' todo has been saved!');
+      // other logic
     };
 
   });
